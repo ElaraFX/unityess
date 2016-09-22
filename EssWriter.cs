3357d0cffbe359a989def2efd79b9fe99a00d71b
WindyPaper <windy_max@163.com>
2016-09-22 17:53:39 +0800

    Ess Export commit

--- EssWriter.cs
+++ EssWriter.cs
@@ -22,39 +22,22 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class EssWriter : MonoBehaviour {
-    string essDataString; /**< ess字符流数据 */
+public class EssWriter {
+    StringBuilder essDataString = new StringBuilder(); /**< ess字符流数据 */
 
     public bool writeToFile = false;
     string path = ""; /**< ess写入文件路径 */
     string essFileName = "";
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
     
     public StreamWriter CreateFile(string path, string name)
     {
         StreamWriter sw;
-        Debug.Log("path = " + path);
-        FileInfo t = new FileInfo(path + "//" + name);
-        if (!t.Exists)
+        FileInfo t = new FileInfo(path + "/" + name);
+        if (t.Exists)
         {
-            /**< 如果此文件不存在则创建 */
-            sw = t.CreateText();
-        }
-        else
-        {
-            /**< 如果此文件存在删除内容 */
             t.Delete();
-            sw = t.CreateText();
         }
+        sw = t.CreateText();
         return sw;
     }
 
@@ -83,65 +66,65 @@ public class EssWriter : MonoBehaviour {
 
 	public void BeginNode(string type, string name)
     {        
-        essDataString += String.Format("\nnode \"{0}\" \"{1}\"", type, name);
+        essDataString.Append(String.Format("\nnode \"{0}\" \"{1}\"", type, name));
     }
 
 	public void LinkParam(string input, string shader, string output)
     {
-        essDataString += String.Format("\n\tparam_link \"{0}\" \"{1}\" \"{2}\"", input, shader, output);
+        essDataString.Append(String.Format("\n\tparam_link \"{0}\" \"{1}\" \"{2}\"", input, shader, output));
     }
 
 	public void AddScaler(string name, float value)
     {
-        essDataString += String.Format("\n\tscalar \"{0}\" {1}", name, value);
+        essDataString.Append(String.Format("\n\tscalar \"{0}\" {1}", name, value));
     }
 
 	public void AddInt(string name, int value)
     {
-        essDataString += String.Format("\n\tint \"{0}\" {1}", name, value);
+        essDataString.Append(String.Format("\n\tint \"{0}\" {1}", name, value));
     }
 
 	public void AddVector4(string name, Vector4 value)
     {
-        essDataString += String.Format("\n\tvector4 \"{0}\" {1} {2} {3} {4}", name, value.x, value.y, value.z, value.w);
+        essDataString.Append(String.Format("\n\tvector4 \"{0}\" {1} {2} {3} {4}", name, value.x, value.y, value.z, value.w));
     }
 
 	public void AddVector3(string name, Vector3 value)
     {
-        essDataString += String.Format("\n\tvector \"{0}\" {1} {2} {3}", name, value.x, value.y, value.z);
+        essDataString.Append(String.Format("\n\tvector \"{0}\" {1} {2} {3}", name, value.x, value.y, value.z));
     }
 
 	public void AddToken(string name, string value)
     {
-        essDataString += String.Format("\n\ttoken \"{0}\" {1}", name, value);
+        essDataString.Append(String.Format("\n\ttoken \"{0}\" {1}", name, value));
     }
 
 	public void AddColor(string name, Vector4 value)
     {
-        essDataString += String.Format("\n\tcolor \"{0}\" {1} {2} {3}", name, value.x * value.w, value.y * value.w, value.z * value.w);
+        essDataString.Append(String.Format("\n\tcolor \"{0}\" {1} {2} {3}", name, value.x * value.w, value.y * value.w, value.z * value.w));
     }
 
 	public void AddColor(string name, Vector3 value)
     {
-        essDataString += String.Format("\n\tcolor \"{0}\" {1} {2} {3}", name, value.x, value.y, value.z);
+        essDataString.Append(String.Format("\n\tcolor \"{0}\" {1} {2} {3}", name, value.x, value.y, value.z));
     }
 
 	public void AddBool(string name, bool value)
     {
-        essDataString += String.Format("\n\tbool \"{0}\" {1}", name, (value ? "on" : "off"));
+        essDataString.Append(String.Format("\n\tbool \"{0}\" {1}", name, (value ? "on" : "off")));
     }
 
 	public void AddRef(string name, string refVal)
     {
-        essDataString += String.Format("\n\tref \"{0}\" \"{1}\"", name, refVal);
+        essDataString.Append(String.Format("\n\tref \"{0}\" \"{1}\"", name, refVal));
     }
 
 	public void AddRefGroup(string grouptype, List<string> refelements)
     {
-        essDataString += String.Format("\n\tref[] \"{0}\" 1", grouptype);
+        essDataString.Append(String.Format("\n\tref[] \"{0}\" 1", grouptype));
         foreach(string var in refelements)
         {
-            essDataString += String.Format("\n\t\t\"{0}\"", var);
+            essDataString.Append(String.Format("\n\t\t\"{0}\"", var));
         }
     }
 
@@ -153,36 +136,36 @@ public class EssWriter : MonoBehaviour {
 		    for (int col = 0; col < 4; ++col)
 		    {   
                 float val = matrix[row, col];
-			    //matStr += matrix[row,col].ToString() << " ";
+			    //matStr.Append(matrix[row,col].ToString() << " ";
                 matStr = matStr + val + " ";
 		    }		
 	    }
-        essDataString += String.Format("\n\tmatrix \"{0}\" {1}", name, matStr);
+        essDataString.Append(String.Format("\n\tmatrix \"{0}\" {1}", name, matStr));
     }
 
 	public void AddEnum(string name, string value)
     {
-        essDataString += String.Format("\n\tenum \"{0}\" \"{1}\"", name, value);
+        essDataString.Append(String.Format("\n\tenum \"{0}\" \"{1}\"", name, value));
     }
 
 	public void AddRenderCommand(string inst_group_name, string cam_name, string option_name)
     {
-        essDataString += String.Format("\nrender \"{0}\" \"{1}\" \"{2}\"", inst_group_name, cam_name, option_name);
+        essDataString.Append(String.Format("\nrender \"{0}\" \"{1}\" \"{2}\"", inst_group_name, cam_name, option_name));
     }
 
 	public void AddDeclare()
     {
-        essDataString += String.Format("\n\tdeclare");
+        essDataString.Append(String.Format("\n\tdeclare"));
     }
 
 	public void AddDeclare(string type, string name, string storage_class)
     {
-        essDataString += String.Format("\n\tdeclare {0} \"{1}\" {2}", type, name, storage_class);
+        essDataString.Append(String.Format("\n\tdeclare {0} \"{1}\" {2}", type, name, storage_class));
     }
 
     public void AddIndexArray(string name, int[] indexs, bool faceVarying)
     {
-        essDataString += String.Format("\n\tindex[] \"{0}\" 1", name);
+        essDataString.Append(String.Format("\n\tindex[] \"{0}\" 1", name));
 
         Assert.IsTrue(faceVarying == false); //暂时不加faceVarying
 
@@ -191,31 +174,31 @@ public class EssWriter : MonoBehaviour {
 		{            
             lineIndexDataStr = lineIndexDataStr + indexs[i] + " ";
 		}
-        essDataString += String.Format("\n\t\t{0}", lineIndexDataStr);
+        essDataString.Append(String.Format("\n\t\t{0}", lineIndexDataStr));
 
     }
 
     public void AddPointArray(string name, Vector3[] vectorArray)
     {
-        essDataString += String.Format("\n\tpoint[] \"{0}\" 1", name);
+        essDataString.Append(String.Format("\n\tpoint[] \"{0}\" 1", name));
         for(int i = 0; i < vectorArray.Length; ++i)
         {
-            essDataString += String.Format("\n\t\t {0} {1} {2}", vectorArray[i].x, vectorArray[i].y, vectorArray[i].z);
+            essDataString.Append(String.Format("\n\t\t {0} {1} {2}", vectorArray[i].x, vectorArray[i].y, vectorArray[i].z));
         }
     }
 
     public void AddCustomString(string str)
     {
-        essDataString += String.Format("\n{0}", str);
+        essDataString.Append(String.Format("\n{0}", str));
     }
 
     public void EndNode()
     {
-        essDataString += "\nend";
+        essDataString.Append("\nend");
     }
 
     public string getEssDataString()
     {
-        return essDataString;
+        return essDataString.ToString();
     }
 }
--- ExportESS.cs
+++ ExportESS.cs
@@ -15,25 +15,41 @@
  *************************************************************************/
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExportESS : MonoBehaviour {
+public class ExportESS
+{
 
     /**< 需要导出的家具TAG类型 */
-    public string[] exportFurnitureModelTags = new[] { "furniture" };
+    public string[] exportFurnitureModelTags = new[] { "furniture", "wall", "sofa", "window" };
 
     EssWriter essWriter = new EssWriter();
 
     Matrix4x4 l2rMatrix = new Matrix4x4();
 
     List<string> renderInstList = new List<string>();
+
     Dictionary<string, uint> meshMap = new Dictionary<string, uint>(); //meshname - ref count
 
-    public ExportESS(string essPath, string essFileName)
+    private static ExportESS instance = null;
+
+    public static ExportESS Instance
     {
-        essWriter.Initialize(essPath, essFileName);
+        get
+        {
+            if( null == instance )
+            {
+                instance = new ExportESS();
+            }
+
+            return instance;
+        }
+    }
+
+    public ExportESS( string essPath, string essFileName )
+    {
+        essWriter.Initialize( essPath, essFileName );
     }
 
     public ExportESS()
@@ -41,31 +57,20 @@ public class ExportESS : MonoBehaviour {
         essWriter.Initialize();
     }
 
-
-	// Use this for initialization
-	void Start () {
-               
-	}
-
     void resetData()
     {
-        Vector4 v1 = new Vector4(1, 0, 0, 0);
-        Vector4 v2 = new Vector4(0, 1, 0, 0);
-        Vector4 v3 = new Vector4(0, 0, -1, 0);
-        Vector4 v4 = new Vector4(0, 0, 0, 1);
-        l2rMatrix.SetColumn(0, v1);
-        l2rMatrix.SetColumn(1, v2);
-        l2rMatrix.SetColumn(2, v3);
-        l2rMatrix.SetColumn(3, v4);
+        Vector4 v1 = new Vector4( 1, 0, 0, 0 );
+        Vector4 v2 = new Vector4( 0, 1, 0, 0 );
+        Vector4 v3 = new Vector4( 0, 0, -1, 0 );
+        Vector4 v4 = new Vector4( 0, 0, 0, 1 );
+        l2rMatrix.SetColumn( 0, v1 );
+        l2rMatrix.SetColumn( 1, v2 );
+        l2rMatrix.SetColumn( 2, v3 );
+        l2rMatrix.SetColumn( 3, v4 );
 
         renderInstList.Clear();
         meshMap.Clear();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     /** 导出场景接口
      * \return string ess的字符流数据
@@ -78,30 +83,44 @@ public class ExportESS : MonoBehaviour {
 
         addGlobalEnvLight();
 
-        Camera cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        addCameraData(cam);
+        Camera cam = Camera.main;
+
+        if( null == cam )
+        {
+            Debug.LogError( "the scene is not exist mainCamera" );
+
+            return null;
+        }
+
+        addCameraData( cam );
 
         addDefaultMtl();
 
+        // To do
         GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
-        foreach (GameObject gameObj in allObjects)
+
+        // GameObject[] allObjects = ParentNodeManager.Instance.GetChildObjArray();
+
+        foreach( GameObject gameObj in allObjects )
         {
-            int findRet = Array.IndexOf(exportFurnitureModelTags, gameObj.tag);
-            if(findRet >= 0)
+            int findRet = Array.IndexOf( exportFurnitureModelTags, gameObj.tag );
+
+            if( findRet >= 0 )
             {
-                MeshFilter viewedModelFilter = (MeshFilter)gameObj.GetComponent("MeshFilter");
-                if(viewedModelFilter)
+                MeshFilter viewedModelFilter = (MeshFilter)gameObj.GetComponent( "MeshFilter" );
+
+                if( viewedModelFilter )
                 {
                     Mesh exportMesh = viewedModelFilter.mesh;
                     Vector3[] vertexs = exportMesh.vertices;
-                    int[] indexs = exportMesh.GetIndices(0);
-                         
+                    int[] indexs = exportMesh.GetIndices( 0 );
+
                     Matrix4x4 objMat = gameObj.transform.localToWorldMatrix;
                     objMat = l2rMatrix * objMat;
 
-                    addRenderInst(gameObj.name, objMat.transpose, vertexs, indexs);                
+                    addRenderInst( gameObj.name, objMat.transpose, vertexs, indexs );
                 }
-                        
+
             }
         }
 
@@ -113,126 +132,130 @@ public class ExportESS : MonoBehaviour {
         return essWriter.getEssDataString();
     }
 
-    void addCameraData(Camera cam)
+    void addCameraData( Camera cam )
     {
-        essWriter.BeginNode("camera", "cam1");
-        essWriter.AddRef("env_shader", "environment_shader");
-        essWriter.AddScaler("aspect", cam.aspect);
-        essWriter.AddScaler("focal", 50.0f);
-        essWriter.AddScaler("aperture", 144.724029f);
-        essWriter.AddInt("res_x", cam.pixelWidth);
-        essWriter.AddInt("res_y", cam.pixelHeight);
+        essWriter.BeginNode( "camera", "cam1" );
+        essWriter.AddRef( "env_shader", "environment_shader" );
+        essWriter.AddScaler( "aspect", cam.aspect );
+        essWriter.AddScaler( "focal", 50.0f );
+        essWriter.AddScaler( "aperture", 144.724029f );
+        essWriter.AddInt( "res_x", cam.pixelWidth );
+        essWriter.AddInt( "res_y", cam.pixelHeight );
         essWriter.EndNode();
 
-        essWriter.BeginNode("instance", "caminst1");
-        essWriter.AddRef("element", "cam1");
+        essWriter.BeginNode( "instance", "caminst1" );
+        essWriter.AddRef( "element", "cam1" );
         Matrix4x4 camMat = cam.transform.localToWorldMatrix;
         camMat = l2rMatrix * camMat * l2rMatrix;
-        essWriter.AddMatrix("transform", camMat.transpose);
-        essWriter.AddMatrix("motion_transform", camMat.transpose);
+        essWriter.AddMatrix( "transform", camMat.transpose );
+        essWriter.AddMatrix( "motion_transform", camMat.transpose );
         essWriter.EndNode();
     }
 
     void addEssOption()
     {
-        essWriter.BeginNode("options", "opt");
-        essWriter.AddEnum("filter", "gaussian");
-        essWriter.AddScaler("filter_size", 3.0f);
-        essWriter.AddScaler("display_gamma", 2.2f);
+        essWriter.BeginNode( "options", "opt" );
+        essWriter.AddEnum( "filter", "gaussian" );
+        essWriter.AddScaler( "filter_size", 3.0f );
+        essWriter.AddScaler( "display_gamma", 2.2f );
         essWriter.EndNode();
-    }    
+    }
 
-    void addDirLight(Light light)
+    void addDirLight( Light light )
     {
         //暂时不需要导出灯光
-        essWriter.BeginNode("directlight", "dir_light");
-        essWriter.AddScaler("intensity", light.intensity);
-        essWriter.AddEnum("face", "front");
-	    essWriter.AddColor("color", light.color);
+        essWriter.BeginNode( "directlight", "dir_light" );
+        essWriter.AddScaler( "intensity", light.intensity );
+        essWriter.AddEnum( "face", "front" );
+        essWriter.AddColor( "color", light.color );
         essWriter.EndNode();
-        
+
         //TODO:需要添加transform矩阵
     }
 
     void addGlobalEnvLight()
     {
-        essWriter.BeginNode("output_result", "global_environment");
-        Vector3 envColor = new Vector3(0.42f, 0.80f, 0.98f);
-        essWriter.AddColor("input", envColor);
-        essWriter.AddBool("env_emits_GI", true);
+        essWriter.BeginNode( "output_result", "global_environment" );
+        Vector3 envColor = new Vector3( 0.42f, 0.80f, 0.98f );
+        essWriter.AddColor( "input", envColor );
+        essWriter.AddBool( "env_emits_GI", true );
         essWriter.EndNode();
 
-        essWriter.BeginNode("osl_shadergroup", "environment_shader");
+        essWriter.BeginNode( "osl_shadergroup", "environment_shader" );
         List<string> refList = new List<string>();
-        refList.Add("global_environment");
-        essWriter.AddRefGroup("nodes", refList);
+        refList.Add( "global_environment" );
+        essWriter.AddRefGroup( "nodes", refList );
         essWriter.EndNode();
     }
 
     void addDefaultMtl()
     {
-        essWriter.BeginNode("max_ei_standard", "standard_shader");
+        essWriter.BeginNode( "max_ei_standard", "standard_shader" );
         essWriter.EndNode();
 
-        essWriter.BeginNode("max_result", "result_stand_shader");
-        essWriter.AddCustomString("\tparam_link \"input\" \"standard_shader\" \"result\"");
+        essWriter.BeginNode("backface_cull", "backface_shader");
+        essWriter.LinkParam("material", "standard_shader", "result");
         essWriter.EndNode();
 
-        essWriter.BeginNode("osl_shadergroup", "stand_shader_group");
+        essWriter.BeginNode( "max_result", "result_stand_shader" );
+        essWriter.LinkParam("input", "backface_shader", "result");
+        essWriter.EndNode();
+
+        essWriter.BeginNode( "osl_shadergroup", "stand_shader_group" );
         List<string> groups = new List<string>();
-        groups.Add("result_stand_shader");
-        essWriter.AddRefGroup("nodes", groups);
+        groups.Add( "result_stand_shader" );
+        essWriter.AddRefGroup( "nodes", groups );
         essWriter.EndNode();
 
-        essWriter.BeginNode("material", "standard_mtl");
-        essWriter.AddRef("surface_shader", "stand_shader_group");
+        essWriter.BeginNode( "material", "standard_mtl" );
+        essWriter.AddRef( "surface_shader", "stand_shader_group" );
         essWriter.EndNode();
     }
 
-    void addEssMeshData(string nodeName, Vector3[] vertexs, int[] indexs)
+    void addEssMeshData( string nodeName, Vector3[] vertexs, int[] indexs )
     {
-        essWriter.BeginNode("poly", nodeName);
-        essWriter.AddPointArray("pos_list", vertexs);
-        essWriter.AddIndexArray("triangle_list", indexs, false);
+        essWriter.BeginNode( "poly", nodeName );
+        essWriter.AddPointArray( "pos_list", vertexs );
+        essWriter.AddIndexArray( "triangle_list", indexs, false );
         essWriter.EndNode();
     }
 
-    void addRenderInst(string meshName, Matrix4x4 transform, Vector3[] vertexs, int[] indexs)
+    void addRenderInst( string meshName, Matrix4x4 transform, Vector3[] vertexs, int[] indexs )
     {
         string nodeName = meshName + "_node";
-        if(!meshMap.ContainsKey(meshName))
+        if( !meshMap.ContainsKey( meshName ) )
         {
-            addEssMeshData(nodeName, vertexs, indexs);
-            meshMap.Add(meshName, 1);
-        }        
+            addEssMeshData( nodeName, vertexs, indexs );
+            meshMap.Add( meshName, 1 );
+        }
         else
         {
             meshMap[meshName] += 1;
             meshName += meshMap[meshName];
         }
 
-        essWriter.BeginNode("instance", meshName);
-        essWriter.AddRef("element", nodeName);
+        essWriter.BeginNode( "instance", meshName );
+        essWriter.AddRef( "element", nodeName );
         List<string> mtlList = new List<string>();
-        mtlList.Add("standard_mtl");
-        essWriter.AddRefGroup("mtl_list", mtlList);        
-        essWriter.AddMatrix("transform", transform);
-        essWriter.AddMatrix("motion_transform", transform);
+        mtlList.Add( "standard_mtl" );
+        essWriter.AddRefGroup( "mtl_list", mtlList );
+        essWriter.AddMatrix( "transform", transform );
+        essWriter.AddMatrix( "motion_transform", transform );
         essWriter.EndNode();
 
-        renderInstList.Add(meshName);
+        renderInstList.Add( meshName );
     }
 
     void addInstanceGroup()
     {
-        essWriter.BeginNode("instgroup", "world");        
-        renderInstList.Add("caminst1");
-        essWriter.AddRefGroup("instance_list", renderInstList);
+        essWriter.BeginNode( "instgroup", "world" );
+        renderInstList.Add( "caminst1" );
+        essWriter.AddRefGroup( "instance_list", renderInstList );
         essWriter.EndNode();
     }
 
     void addRenderCommand()
     {
-        essWriter.AddRenderCommand("world", "caminst1", "opt");
+        essWriter.AddRenderCommand( "world", "caminst1", "opt" );
     }
 }
