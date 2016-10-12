7d8032a907602fe5b7dd29c6bd0dc4abb8679149
WindyPaper <windy_max@163.com>
2016-10-12 18:28:32 +0800

    add uv scale

--- ExportESS.cs
+++ ExportESS.cs
@@ -41,6 +41,11 @@ public class ExportESS
 
     /**< 墙纸地板纹理默认类型 */
     string WALL_FLOOR_TEX_TYPE = ".jpg";
+    int exportTexNum = 0;
+
+    /**< 窗户TAG类型 */
+    string WINDOW_TYPE_TAG = "window";
+    int portalLightNum = 0;
 
     EssWriter essWriter = new EssWriter();
 
@@ -50,7 +55,7 @@ public class ExportESS
 
     Dictionary<string, uint> meshMap = new Dictionary<string, uint>(); //meshname - ref count
 
-    Dictionary<string, uint> texMap = new Dictionary<string, uint>(); //texture name - ref count
+    //Dictionary<string, uint> texMap = new Dictionary<string, uint>(); //texture name - ref count
 
     private static ExportESS instance = null;
 
@@ -88,6 +93,9 @@ public class ExportESS
         l2rMatrix.SetColumn( 2, v3 );
         l2rMatrix.SetColumn( 3, v4 );
 
+        portalLightNum = 0;
+        exportTexNum = 0;
+
         renderInstList.Clear();
         meshMap.Clear();
     }
@@ -114,7 +122,7 @@ public class ExportESS
 
         addCameraData( cam );
 
-        addWallFloorTexCoord();
+        //addWallFloorTexCoord();
 
         //addDefaultMtl();
 
@@ -127,10 +135,13 @@ public class ExportESS
 
         //GameObject[] allObjects = ParentNodeManager.Instance.GetChildObjArray();
         GameObject[] furObjs = GameObject.FindGameObjectsWithTag("furniture");
+        GameObject[] windows = GameObject.FindGameObjectsWithTag("window");
         GameObject[] wallObjs = GameObject.FindGameObjectsWithTag("wall");
-        GameObject[] allObjects = new GameObject[furObjs.Length + wallObjs.Length];
+        GameObject[] allObjects = new GameObject[furObjs.Length + wallObjs.Length + windows.Length];
         furObjs.CopyTo(allObjects, 0);
         wallObjs.CopyTo(allObjects, furObjs.Length);
+        int windowStartIndex = furObjs.Length + wallObjs.Length;
+        windows.CopyTo(allObjects, windowStartIndex);
 
         foreach(GameObject gameObj in allObjects)
         {
@@ -156,8 +167,9 @@ public class ExportESS
                     objMat = l2rMatrix * objMat;
 
                     Vector2[] uvs = exportMesh.uv;
-                    string useMtlName = addDefaultMtl(gameObj.GetComponent<Renderer>().material.mainTexture.name);
-                    addVertexRenderInst(regularMeshName(gameObj.name), objMat.transpose, vertexs, indexs, uvs, useMtlName);
+                    Vector2 uvScale = getUVScale(gameObj.transform.localScale);
+                    string useMtlName = addDefaultMtl(gameObj.GetComponent<Renderer>().material.mainTexture.name, uvScale);
+                    addVertexRenderInst(regularMeshName(gameObj.name), objMat.transpose, vertexs, indexs, uvs, useMtlName);                    
 
                     FindChild(gameObj.transform);
                 }
@@ -171,6 +183,18 @@ public class ExportESS
                     Matrix4x4 objMat = gameObj.transform.localToWorldMatrix;
                     objMat = l2rMatrix * objMat;
                     addMappingInst(regularMeshName(meshName), objMat.transpose);
+
+                    //portal light create from window
+                    if(gameObj.tag == WINDOW_TYPE_TAG)
+                    {
+                        int offsetZ = 30;
+                        float portalLightIntensity = 1.0f;
+                        gameObj.transform.localScale = Vector3.one / ESS_SCALE_EFFI;
+                        gameObj.transform.Rotate(-Vector3.right * ESS_ROTATE_ON_XAXIS_DEGREE);
+                        gameObj.transform.Translate(-Vector3.forward * offsetZ);
+                        Matrix4x4 portalLightMat = l2rMatrix * gameObj.transform.localToWorldMatrix * l2rMatrix;
+                        addPortalLight(200, 200, portalLightMat.transpose, portalLightIntensity);
+                    }                    
                 }
             }
         }
@@ -204,7 +228,8 @@ public class ExportESS
                     Matrix4x4 objMat = childTrans.localToWorldMatrix;
                     objMat = l2rMatrix * objMat;
 
-                    string useMtlName = addDefaultMtl(childTrans.GetComponent<Renderer>().material.mainTexture.name);
+                    Vector2 uvScale = getUVScale(childTrans.localScale);
+                    string useMtlName = addDefaultMtl(childTrans.GetComponent<Renderer>().material.mainTexture.name, uvScale);
                     addVertexRenderInst(regularMeshName(childTrans.gameObject.name), objMat.transpose, vertexs, indexs, uvs, useMtlName);
                 }
 
@@ -212,6 +237,28 @@ public class ExportESS
             }
 
         }
+    }
+
+    Vector2 getUVScale(Vector3 scale)
+    {
+        Vector2 uvScale = new Vector2();
+        if(scale.x < 1.0f)
+        {
+            uvScale.x = scale.y;
+            uvScale.y = scale.z;
+        }
+        else if(scale.y < 1.0)
+        {
+            uvScale.x = scale.x;
+            uvScale.y = scale.z;
+        }
+        else if(scale.z < 1.0f)
+        {
+            uvScale.x = scale.x;
+            uvScale.y = scale.y;
+        }
+
+        return uvScale;
     }
 
     string regularMeshName(string inMeshName)
@@ -273,6 +320,36 @@ public class ExportESS
         renderInstList.Add(instanceName);
     }
 
+    void addPortalLight(int width, int height, Matrix4x4 tran, float intensity)
+    {
+        string portalLightNode = "portal_light_node";
+
+        if(portalLightNum == 0)
+        {
+            essWriter.BeginNode("portallight", portalLightNode);
+            essWriter.AddRef("map", "environment_shader");
+            essWriter.AddColor("color", Vector3.one);
+            essWriter.AddScaler("width", width);
+            essWriter.AddScaler("height", height);
+            essWriter.AddScaler("intensity", intensity);
+            essWriter.AddInt("samples", 16);
+            essWriter.EndNode();
+        }        
+
+        string portalLightInst = "portal_light_inst" + portalLightNum;
+        essWriter.BeginNode("instance", portalLightInst);
+        essWriter.AddRef("element", portalLightNode);
+        essWriter.AddBool("visible_primary", false);
+        essWriter.AddBool("cast_shadow", false);
+        essWriter.AddMatrix("transform", tran);
+        essWriter.AddMatrix("motion_transform", tran);
+        essWriter.EndNode();
+
+        renderInstList.Add(portalLightInst);
+
+        portalLightNum += 1;
+    }
+
     string addHDRIEnvMapShader(string hdriFileName, float rotation, float intensity)
     {
         string hdriBaseName = "hdri_env";
@@ -307,10 +384,10 @@ public class ExportESS
 
     void addGlobalEnvLight(string hdrImageName)
     {
-        string hdriShaderName = addHDRIEnvMapShader(hdrImageName, 0, 20.0f);
+        string hdriShaderName = addHDRIEnvMapShader(hdrImageName, 0, 2.0f);
         essWriter.BeginNode("output_result", "global_environment");
         essWriter.LinkParam("input", hdriShaderName, "result");
-        essWriter.AddBool("env_emits_GI", true);
+        essWriter.AddBool("env_emits_GI", false);
         essWriter.EndNode();
 
         essWriter.BeginNode( "osl_shadergroup", "environment_shader" );
@@ -320,71 +397,88 @@ public class ExportESS
         essWriter.EndNode();
     }
 
-    void addWallFloorTexCoord()
+    void addWallFloorTexCoord(string uvgenName, Vector2 uvScale)
     {        
-        essWriter.BeginNode("max_stduv", "wall_floor_uvgen");
+        essWriter.BeginNode("max_stduv", uvgenName);
 	    essWriter.AddToken("mapChannel", "uv0");
         essWriter.AddScaler("uOffset", 0.0f);
-	    essWriter.AddScaler("uScale", 1.0f);
-	    essWriter.AddBool("uWrap", true);
-	    essWriter.AddScaler("vScale", 1.0f);
+        essWriter.AddScaler("uScale", uvScale.x);
+        essWriter.AddScaler("vScale", uvScale.y);
+	    essWriter.AddBool("uWrap", true);	    
 	    essWriter.AddBool("vWrap", true);
 	    essWriter.AddInt("slotType", 1);
-	    essWriter.AddInt("coordMapping", 1);
+	    essWriter.AddInt("coordMapping", 4);
+        essWriter.AddInt("uvwSource", 0);
+        essWriter.AddBool("hideMapBack", false);
+        essWriter.AddBool("uMirror", false);
+        essWriter.AddBool("vMirror", false);
+        essWriter.AddScaler("uAngle", 0.0f);
+        essWriter.AddScaler("vAngle", 0.0f);
+        essWriter.AddScaler("wAngle", 0.0f);
+        essWriter.AddInt("axis", 0);
+        essWriter.AddBool("clip", true);
+        essWriter.AddScaler("blur", 1.0f);
+        essWriter.AddScaler("blurOffset", 0.0f);
+        essWriter.AddBool("uvNoise", false);
+        essWriter.AddBool("uvNoiseAnimate", false);
+        essWriter.AddScaler("uvNoiseAmount", 1.0f);
+        essWriter.AddScaler("uvNoiseSize", 1.0f);
+        essWriter.AddInt("uvNoiseLevel", 1);
+        essWriter.AddScaler("uvNoisePhase", 0.0f);
+        essWriter.AddBool("realWorldScale", false);
 	    essWriter.EndNode();
     }
 
-    string addDefaultMtl(string texName)
+    string addDefaultMtl(string texName, Vector2 uvScale)
     {
         //bitmap         
         string defaultMtlName = "standard_mtl";
-        if(!texMap.ContainsKey(texName))
-        {            
-            int currIndex = texMap.Count;
-            string texNode = texName.Split('.')[0] + "_texnode";
-            essWriter.BeginNode("max_bitmap", texNode);
-            essWriter.LinkParam("tex_coords", "wall_floor_uvgen", "result");
-            essWriter.AddToken("tex_fileName", texName + WALL_FLOOR_TEX_TYPE);
-            essWriter.AddInt("tex_alphaSource", 0);
-            essWriter.EndNode();
+        string defaultUvGenName = "wall_floor_uvgen";
 
-            string stdShadar = "standard_shader" + currIndex;
-            essWriter.BeginNode( "max_ei_standard", stdShadar);
-            essWriter.AddScaler("specular_weight", 0.0f);
-            essWriter.AddScaler("diffuse_weight", 0.9f);
-            essWriter.LinkParam("diffuse_color", texNode, "result");
-            essWriter.EndNode();
+        int currIndex = exportTexNum;
+        string texNode = texName.Split('.')[0] + "_texnode";
 
-            string backfaceShader = "backface_shader" + currIndex;
-            essWriter.BeginNode("backface_cull", backfaceShader);
-            essWriter.LinkParam("material", stdShadar, "result");
-            essWriter.EndNode();
+        string uvGenName = defaultUvGenName + currIndex;
+        addWallFloorTexCoord(uvGenName, uvScale);
 
-            string resultStdShader = "result_stand_shader" + currIndex;
-            essWriter.BeginNode("max_result", resultStdShader);
-            essWriter.LinkParam("input", backfaceShader, "result");
-            essWriter.EndNode();
+        essWriter.BeginNode("max_bitmap", texNode);
+        essWriter.LinkParam("tex_coords", uvGenName, "result");
+        essWriter.AddToken("tex_fileName", texName + WALL_FLOOR_TEX_TYPE);
+        essWriter.AddInt("tex_alphaSource", 0);
+        essWriter.EndNode();
 
-            string stdShaderGroup = "stand_shader_group" + currIndex;
-            essWriter.BeginNode("osl_shadergroup", stdShaderGroup);
-            List<string> groups = new List<string>();
-            groups.Add( resultStdShader );
-            essWriter.AddRefGroup( "nodes", groups );
-            essWriter.EndNode();
+        string stdShadar = "standard_shader" + currIndex;
+        essWriter.BeginNode( "max_ei_standard", stdShadar);
+        essWriter.AddScaler("specular_weight", 0.0f);
+        essWriter.AddScaler("diffuse_weight", 0.9f);
+        essWriter.LinkParam("diffuse_color", texNode, "result");
+        essWriter.EndNode();
 
-            string useMtlName = defaultMtlName + currIndex;
-            essWriter.BeginNode( "material",  useMtlName);
-            essWriter.AddRef("surface_shader", stdShaderGroup);
-            essWriter.EndNode();
+        string backfaceShader = "backface_shader" + currIndex;
+        essWriter.BeginNode("backface_cull", backfaceShader);
+        essWriter.LinkParam("material", stdShadar, "result");
+        essWriter.EndNode();
 
-            texMap.Add(texName, (uint)currIndex);
+        string resultStdShader = "result_stand_shader" + currIndex;
+        essWriter.BeginNode("max_result", resultStdShader);
+        essWriter.LinkParam("input", backfaceShader, "result");
+        essWriter.EndNode();
 
-            return useMtlName;
-        }
-        else
-        {
-            return defaultMtlName + texMap[texName];
-        }
+        string stdShaderGroup = "stand_shader_group" + currIndex;
+        essWriter.BeginNode("osl_shadergroup", stdShaderGroup);
+        List<string> groups = new List<string>();
+        groups.Add( resultStdShader );
+        essWriter.AddRefGroup( "nodes", groups );
+        essWriter.EndNode();
+
+        string useMtlName = defaultMtlName + currIndex;
+        essWriter.BeginNode( "material",  useMtlName);
+        essWriter.AddRef("surface_shader", stdShaderGroup);
+        essWriter.EndNode();
+
+        exportTexNum += 1;
+
+        return useMtlName;
         
     }
 
